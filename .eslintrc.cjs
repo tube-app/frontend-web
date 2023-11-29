5b0adc8bdcc52bdcd1d929e07e68bbc29ff669e7
tuboihirokidesu <tuboi.hiroki1108@gmail.com>
2023-11-29 11:18:24 +0900

    🔨 eslint,prettier の追加

---
+++ .eslintrc.cjs
@@ -0,0 +1,45 @@
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+  },
+  plugins: ["@typescript-eslint", "tailwindcss"],
+  extends: [
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "prettier",
+    "plugin:tailwindcss/recommended",
+  ],
+  rules: {
+    "@typescript-eslint/array-type": "off",
+    "@typescript-eslint/consistent-type-definitions": "off",
+
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      {
+        prefer: "type-imports",
+        fixStyle: "inline-type-imports",
+      },
+    ],
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-misused-promises": [
+      2,
+      {
+        checksVoidReturn: { attributes: false },
+      },
+    ],
+  },
+  settings: {
+    tailwindcss: {
+      callees: ["cn", "cva"],
+      config: "./tailwind.config.ts",
+      classRegex: "^(class(Name)?|tw)$",
+    },
+    next: {
+      rootDir: ["./"],
+    },
+  },
+}
+
+module.exports = config
