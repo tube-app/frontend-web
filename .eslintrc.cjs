d4d2b7c4bf6bd88cdb94aa8185126a64928d9caa
tuboihirokidesu <tuboi.hiroki1108@gmail.com>
2023-12-05 18:57:50 +0900

    🐛

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -24,7 +24,7 @@ const config = {
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
     "@typescript-eslint/no-misused-promises": [
-      2,
+      "error",
       {
         checksVoidReturn: { attributes: false },
       },
