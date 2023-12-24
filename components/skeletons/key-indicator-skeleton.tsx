1c921b138e103c1d4ef5400de399b8d924becf84
Shirahamah <hiropi1224s@gmail.com>
2023-12-24 13:29:24 +0900

    🔥 コメント削除

--- components/skeletons/key-indicator-skeleton.tsx
+++ components/skeletons/key-indicator-skeleton.tsx
@@ -1,38 +1,27 @@
 import { Card, CardContent, CardHeader } from "../ui/card"
 import { Skeleton } from "../ui/skeleton"
 
-export function KeyIndicatorSkeleton() {
+export function KeyIndicatorSkeleton({ count }: { count: number }) {
   return (
     <div className="flex flex-col gap-2">
-      <Card className="gap-2 space-y-1 px-3 py-2">
-        <CardHeader className="p-0">
-          <Skeleton className="h-5 w-48" />
-        </CardHeader>
-        <CardContent className="flex items-center justify-between p-0">
-          <div className="flex items-baseline gap-2">
-            <Skeleton className="h-9 w-16" />
-            <Skeleton className="h-4 w-16" />
-          </div>
-          <div>
-            <Skeleton className="h-8 w-20 rounded-full" />
-          </div>
-        </CardContent>
-      </Card>
-
-      <Card className="gap-2 space-y-1 px-3 py-2">
-        <CardHeader className="p-0">
-          <Skeleton className="h-5 w-48" />
-        </CardHeader>
-        <CardContent className="flex items-center justify-between p-0">
-          <div className="flex items-baseline gap-2">
-            <Skeleton className="h-9 w-16" />
-            <Skeleton className="h-4 w-16" />
-          </div>
-          <div>
-            <Skeleton className="h-8 w-20 rounded-full" />
-          </div>
-        </CardContent>
-      </Card>
+      {Array.from({
+        length: count,
+      }).map((_, i) => (
+        <Card key={i} className="gap-2 space-y-1 px-3 py-2">
+          <CardHeader className="p-0">
+            <Skeleton className="h-5 w-48" />
+          </CardHeader>
+          <CardContent className="flex items-center justify-between p-0">
+            <div className="flex items-baseline gap-2">
+              <Skeleton className="h-9 w-16" />
+              <Skeleton className="h-4 w-16" />
+            </div>
+            <div>
+              <Skeleton className="h-8 w-20 rounded-full" />
+            </div>
+          </CardContent>
+        </Card>
+      ))}
     </div>
   )
 }
