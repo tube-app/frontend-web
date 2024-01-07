5cde83eb2b376696969b5180eafea3629db0f00c
hiropi1224 <hiropi1224s@gmail.com>
2024-01-07 22:45:59 +0900

    ♻️ commentsのモックデータを別ファイルで定義

--- app/comments/_components/comments/comments.stories.tsx
+++ app/comments/_components/comments/comments.stories.tsx
@@ -3,7 +3,7 @@ import { type Meta, type StoryObj } from "@storybook/react"
 import { rest } from "msw"
 import { setupServer } from "msw/node"
 
-import { type CommentThread } from "@/types/api/comment-thread"
+import { coreFanData } from "@/app/api/mock/comments/data"
 
 import { Comments } from "."
 
@@ -43,87 +43,3 @@ export const Default: Story = {
     },
   ],
 }
-
-const coreFanData: CommentThread[] = [
-  {
-    id: "9gy1b3V0dWJlQ34tbWVudElk",
-    kind: "youtube#commentThreadListResponse",
-    tag: "core-fan",
-    title: "lofi hip hop radio - beats to relax/study to",
-    thumbnail: "https://img.youtube.com/vi/5qap5aO4i9A/hqdefault.jpg",
-    commentAuthorName: "Example User: core-fan",
-    commentAuthorIcon: "https://img.youtube.com/vi/5qap5aO4i9A/hqdefault.jpg",
-    commentAuthorChannelUrl: "http://www.youtube.com/channel/UC-Example",
-    commentContent:
-      "lorem ipsum dolor sit amet consectetur adipiscing elit justo",
-    likeCount: 10,
-    isLiked: true,
-    replies: {
-      comments: [
-        {
-          id: "9gy1b3V0dWJlQ34tbWVudElk_1",
-          kind: "youtube#comment",
-          tag: "reply",
-          snippet: {
-            authorDisplayName: "Reply User 1",
-            authorProfileImageUrl: "http://example.com/reply1.jpg",
-            authorChannelUrl: "http://www.youtube.com/channel/UC-Reply1",
-            authorChannelId: {
-              value: "UC-Reply1",
-            },
-            videoId: "dQw4w9WgXcQ",
-            textDisplay: "This is a reply to the example comment.",
-            textOriginal: "This is a reply to the example comment.",
-            parentId: "Ugy1b3V0dWJlQ29tbWVudElk",
-            canRate: true,
-            viewerRating: "none",
-            likeCount: 5,
-            publishedAt: "2023-12-15T13:00:00Z",
-            updatedAt: "2023-12-15T13:00:00Z",
-          },
-        },
-      ],
-    },
-  },
-  {
-    id: "Ugy1b3V0dWJlQ39tbWVudElk",
-    kind: "youtube#commentThreadListResponse",
-    tag: "core-fan",
-    title: "lofi hip hop radio - beats to relax/study to",
-    thumbnail: "https://img.youtube.com/vi/5qap5aO4i9A/hqdefault.jpg",
-    commentAuthorName: "Example User: core-fan",
-    commentAuthorIcon: "https://img.youtube.com/vi/5qap5aO4i9A/hqdefault.jpg",
-    commentAuthorChannelUrl: "http://www.youtube.com/channel/UC-Example",
-    commentContent:
-      "lorem ipsum dolor sit amet consectetur adipiscing elit justo",
-    likeCount: 10,
-    isLiked: true,
-    replies: {
-      comments: [
-        {
-          id: "Ugy1b3V0dWJlQ39tbWVudElk",
-          kind: "youtube#comment",
-          tag: "reply",
-          snippet: {
-            authorDisplayName: "Reply User 22222",
-            authorProfileImageUrl:
-              "https://img.youtube.com/vi/5qap5aO4i9A/hqdefault.jpg",
-            authorChannelUrl: "http://www.youtube.com/channel/UC-Reply1",
-            authorChannelId: {
-              value: "UC-Reply1",
-            },
-            videoId: "dQw4w9WgXcQ",
-            textDisplay: "This is a reply to the example comment.",
-            textOriginal: "This is a reply to the example comment.",
-            parentId: "Ugy1b3V0dWJlQ29tbWVudElk",
-            canRate: true,
-            viewerRating: "none",
-            likeCount: 5,
-            publishedAt: "2023-12-15T13:00:00Z",
-            updatedAt: "2023-12-15T13:00:00Z",
-          },
-        },
-      ],
-    },
-  },
-]
