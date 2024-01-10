3b696ab37c8d2546e027ad47136010bef9eea713
tuboihirokidesu <tuboi.hiroki1108@gmail.com>
2024-01-10 22:21:28 +0900

    ✨ 認証周りのUI実装 & サインイン処理の実装

--- types/index.ts
+++ types/index.ts
@@ -2,3 +2,15 @@ export type SearchParamProps = {
   params: { id: string }
   searchParams: { [key: string]: string | string[] | undefined }
 }
+
+export type ActionsResult =
+  | {
+      isSuccess: true
+      message: string
+    }
+  | {
+      isSuccess: false
+      error: {
+        message: string
+      }
+    }
